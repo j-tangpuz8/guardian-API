39f0b22b43bea859e094c374b3bac861650ad607
j-tangpuz8 <jtangpuz@guardian.ph>
2025-04-01 17:55:36 +0800

    modified: login Controller to unuse bcrypt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -39,11 +39,6 @@ const loginUser = async (req, res) => {
       return res.status(404).json({message: "User not found"});
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({message: "Invalid password"});
-    }
-
     const streamUserId = user._id.toString();
     const token = client.createToken(streamUserId);
 
